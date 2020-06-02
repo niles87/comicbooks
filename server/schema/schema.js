fe0252d7572875fcd78e969204bca109f5edd47b
niles87 <nbing19@gmail.com>
2020-06-01 19:05:44 -0700

    added dummy data to test root queries, added a hero type and added a root query for heroes

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,13 +1,35 @@
 const graphql = require("graphql");
+const _ = require("lodash");
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+// dummy data
+var comics = [
+  { name: "Spider-man", publisher: "Marvel", id: "1" },
+  { name: "Superman", publisher: "DC", id: "2" },
+  { name: "Avengers", publisher: "Marvel", id: "3" },
+];
+
+var heroes = [
+  { name: "Superman", skills: "Kryptonian", id: "1" },
+  { name: "Spiderman", skills: "Spider Strength", id: "2" },
+  { name: "Thor", skills: "Control Lightning", id: "3" },
+];
 
 const ComicBookType = new GraphQLObjectType({
   name: "Comic",
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
-    genre: { type: GraphQLString },
+    publisher: { type: GraphQLString },
+  }),
+});
+
+const HeroType = new GraphQLObjectType({
+  name: "Hero",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    skills: { type: GraphQLString },
   }),
 });
 
@@ -16,9 +38,17 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     comic: {
       type: ComicBookType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        //code to get data from db
+        // code to get data from db
+        return _.find(comics, { id: args.id });
+      },
+    },
+    hero: {
+      type: HeroType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(heroes, { id: args.id });
       },
     },
   },
