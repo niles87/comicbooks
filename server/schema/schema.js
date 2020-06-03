b1bae9203e8505d1d4daab321076f3b63957a4a8
niles87 <nbing19@gmail.com>
2020-06-03 05:50:03 -0700

    updated schema to include references for queries

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,12 +1,15 @@
 const graphql = require("graphql");
 const _ = require("lodash");
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLList } = graphql;
 
 // dummy data
 var comics = [
-  { name: "Spider-man", publisher: "Marvel", id: "1" },
-  { name: "Superman", publisher: "DC", id: "2" },
-  { name: "Avengers", publisher: "Marvel", id: "3" },
+  { name: "Spider-man", publisher: "Marvel", id: "1", heroId: "2" },
+  { name: "Superman", publisher: "DC", id: "2", heroId: "1" },
+  { name: "Avengers", publisher: "Marvel", id: "3", heroId: "3" },
+  { name: "Venom", publisher: "Marvel", id: "4", heroId: "2" },
+  { name: "Justice League", publisher: "DC", id: "5", heroId: "1" },
+  { name: "Dark World", publisher: "Marvel", id: "6", heroId: "3" },
 ];
 
 var heroes = [
@@ -21,6 +24,12 @@ const ComicBookType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     publisher: { type: GraphQLString },
+    hero: {
+      type: HeroType,
+      resolve(parent, args) {
+        return _.find(heroes, { id: parent.heroId });
+      },
+    },
   }),
 });
 
@@ -30,6 +39,12 @@ const HeroType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     skills: { type: GraphQLString },
+    comic: {
+      type: new GraphQLList(ComicBookType),
+      resolve(parent, args) {
+        return _.filter(comics, { heroId: parent.id });
+      },
+    },
   }),
 });
 
@@ -49,6 +64,18 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         return _.find(heroes, { id: args.id });
+      },
+    },
+    comicbooks: {
+      type: new GraphQLList(ComicBookType),
+      resolve(parent, args) {
+        return comics;
+      },
+    },
+    heroes: {
+      type: new GraphQLList(HeroType),
+      resolve(parent, args) {
+        return heroes;
       },
     },
   },
