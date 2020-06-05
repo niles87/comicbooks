0ccfadc58d84ce5f9a557aac7809fee7c6898486
niles87 <nbing19@gmail.com>
2020-06-04 17:40:11 -0700

    added cors for graphql api

--- server/server.js
+++ server/server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+
+app.use(cors());
 
 mongoose.connect(
   `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0-2lmdc.mongodb.net/gql-comics`,
